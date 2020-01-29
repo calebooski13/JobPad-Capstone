2d8f4a1f50e041c8903ff1bccdf67edeb2ff3ab9
Calebooski <mrmeadows2018@gmail.com>
2020-01-29 11:04:38 -0500

    able to create customer

--- JobPad/Controllers/HomeController.cs
+++ JobPad/Controllers/HomeController.cs
@@ -6,25 +6,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using JobPad.Models;
+using Microsoft.AspNetCore.Identity;
+using JobPad.Data;
 
 namespace JobPad.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
+        private UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager
+            )
         {
+            _context = context;
+            _userManager = userManager;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        // Private method to get current user
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+            public IActionResult Index()
         {
+            var currentUser = _userManager.GetUserId(HttpContext.User);
+
+            ViewData["currentUser"] = currentUser;
             return View();
         }
 
         public IActionResult Privacy()
         {
+            var currentUser = _userManager.GetUserId(HttpContext.User);
+
+            ViewData["currentUser"] = currentUser;
             return View();
         }
 
